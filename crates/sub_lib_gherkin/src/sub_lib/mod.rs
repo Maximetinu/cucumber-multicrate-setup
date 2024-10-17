1461968b6e99bd2456fe2b52d9195319bd2633fa
Metinu – Miguel Medina Ballesteros <maximetinu@gmail.com>
2024-10-17 14:41:22 +0200

    This introduces the bug

--- crates/sub_lib_gherkin/src/sub_lib/mod.rs
+++ crates/sub_lib_gherkin/src/sub_lib/mod.rs
@@ -1,12 +1,8 @@
 use cucumber::*;
-use sub_lib::Cat;
 
-// `World` is your shared, likely mutable state.
-// Cucumber constructs it via `Default::default()` for each scenario.
-#[derive(Debug, Default, World)]
-pub struct AnimalWorld {
-    pub(crate) cat: Cat,
-}
+pub mod context;
+
+use context::AnimalWorld;
 
 // Steps are defined with `given`, `when` and `then` attributes.
 #[given("a hungry cat")]

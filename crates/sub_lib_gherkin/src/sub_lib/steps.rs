139ecc281faa256f04378c6ed8c61093ad66e7c7
Metinu – Miguel Medina Ballesteros <maximetinu@gmail.com>
2024-10-17 14:42:53 +0200

    This introduces the bug as well (see 2 latest commits)

---
+++ crates/sub_lib_gherkin/src/sub_lib/steps.rs
@@ -0,0 +1,19 @@
+use cucumber::*;
+
+use super::AnimalWorld;
+
+// Steps are defined with `given`, `when` and `then` attributes.
+#[given("a hungry cat")]
+pub fn hungry_cat(world: &mut AnimalWorld) {
+    world.cat.hungry = true;
+}
+
+#[when("I feed the cat")]
+pub fn feed_cat(world: &mut AnimalWorld) {
+    world.cat.feed();
+}
+
+#[then("the cat is not hungry")]
+pub fn cat_is_fed(world: &mut AnimalWorld) {
+    assert!(!world.cat.hungry);
+}
